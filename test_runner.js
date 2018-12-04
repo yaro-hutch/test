ef7ab4dc29d096f35bac9c85c51bca4d1b8441d0
Yaro Svirsky <yaroslav.svirsky@hutchgames.com>
2018-12-04 12:56:40 +0000

    Test

---
+++ test_runner.js
@@ -0,0 +1,4 @@
+"use strict"
+
+var path = require("path");
+require(path.join(__dirname, "node_modules/jasmine-node/lib/jasmine-node/cli.js"));
---
+++ test_spec.js
@@ -0,0 +1,13 @@
+"use strict"
+
+describe("Config consistancy tests" , function () {
+    it("should succeed", function(done) {
+        expect(true).toBeTruthy();
+        done();
+    });
+
+    it("should fail", function(done) {
+        expect(false).toBeTruthy();
+        done();
+    });
+});
